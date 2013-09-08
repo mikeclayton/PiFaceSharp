d817d1e6ec5c223e3558bbf0966396041fe8d88d
Mike Clayton <mike@kingslandconsulting.co.uk>
2013-09-08 12:19:24 +0100

    Added sample console app

---
+++ src/Kingsland.PiFaceSharp.SampleConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kingsland.PiFaceSharp.Spi;
+
+namespace Kingsland.PiFaceSharp.SampleConsole
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // get reference to default piface device
+            var piface = new PiFaceDevice();
+
+            // toggle each output pin on and off 50 times
+            for (byte pin = 0; pin < 8; pin++)
+            {
+                var state = true;
+                for (var i = 0; i < 50; i++)
+                {
+                    piface.SetOutputPinState(pin, state);
+                    System.Threading.Thread.Sleep(25);
+                    state = !state;
+                }
+            }
+
+            // read the current state of each input pin
+            for (byte pin = 0; pin < 8; pin++)
+            {
+                Console.Write(piface.GetInputPinState(pin) + " ");
+            }
+
+        }
+
+    }
+
+}
