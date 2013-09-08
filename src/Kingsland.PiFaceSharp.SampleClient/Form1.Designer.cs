0ace82b1ce3fce9bcae18c590c5034925930da47
Mike Clayton <mike@kingslandconsulting.co.uk>
2013-09-08 12:15:01 +0100

    Added sample Client project for controlling a remote PiFaceDevice via a PiFaceTcpServer

---
+++ src/Kingsland.PiFaceSharp.SampleClient/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Kingsland.PiFaceSharp.SampleClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxOutputPins = new System.Windows.Forms.GroupBox();
+            this.chkOutputPin0 = new System.Windows.Forms.CheckBox();
+            this.chkOutputPin1 = new System.Windows.Forms.CheckBox();
+            this.chkOutputPin2 = new System.Windows.Forms.CheckBox();
+            this.chkOutputPin3 = new System.Windows.Forms.CheckBox();
+            this.chkOutputPin4 = new System.Windows.Forms.CheckBox();
+            this.chkOutputPin5 = new System.Windows.Forms.CheckBox();
+            this.chkOutputPin6 = new System.Windows.Forms.CheckBox();
+            this.chkOutputPin7 = new System.Windows.Forms.CheckBox();
+            this.gbxOutputPins.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxOutputPins
+            // 
+            this.gbxOutputPins.Controls.Add(this.chkOutputPin7);
+            this.gbxOutputPins.Controls.Add(this.chkOutputPin6);
+            this.gbxOutputPins.Controls.Add(this.chkOutputPin5);
+            this.gbxOutputPins.Controls.Add(this.chkOutputPin4);
+            this.gbxOutputPins.Controls.Add(this.chkOutputPin3);
+            this.gbxOutputPins.Controls.Add(this.chkOutputPin2);
+            this.gbxOutputPins.Controls.Add(this.chkOutputPin1);
+            this.gbxOutputPins.Controls.Add(this.chkOutputPin0);
+            this.gbxOutputPins.Location = new System.Drawing.Point(201, 12);
+            this.gbxOutputPins.Name = "gbxOutputPins";
+            this.gbxOutputPins.Size = new System.Drawing.Size(107, 208);
+            this.gbxOutputPins.TabIndex = 1;
+            this.gbxOutputPins.TabStop = false;
+            this.gbxOutputPins.Text = "Output Pin States";
+            // 
+            // chkOutputPin0
+            // 
+            this.chkOutputPin0.AutoSize = true;
+            this.chkOutputPin0.Location = new System.Drawing.Point(12, 23);
+            this.chkOutputPin0.Name = "chkOutputPin0";
+            this.chkOutputPin0.Size = new System.Drawing.Size(50, 17);
+            this.chkOutputPin0.TabIndex = 1;
+            this.chkOutputPin0.Text = "Pin 0";
+            this.chkOutputPin0.UseVisualStyleBackColor = true;
+            this.chkOutputPin0.CheckedChanged += new System.EventHandler(this.chkOutputPin0_CheckedChanged);
+            // 
+            // chkOutputPin1
+            // 
+            this.chkOutputPin1.AutoSize = true;
+            this.chkOutputPin1.Location = new System.Drawing.Point(12, 46);
+            this.chkOutputPin1.Name = "chkOutputPin1";
+            this.chkOutputPin1.Size = new System.Drawing.Size(50, 17);
+            this.chkOutputPin1.TabIndex = 1;
+            this.chkOutputPin1.Text = "Pin 1";
+            this.chkOutputPin1.UseVisualStyleBackColor = true;
+            this.chkOutputPin1.CheckedChanged += new System.EventHandler(this.chkOutputPin1_CheckedChanged);
+            // 
+            // chkOutputPin2
+            // 
+            this.chkOutputPin2.AutoSize = true;
+            this.chkOutputPin2.Location = new System.Drawing.Point(12, 69);
+            this.chkOutputPin2.Name = "chkOutputPin2";
+            this.chkOutputPin2.Size = new System.Drawing.Size(50, 17);
+            this.chkOutputPin2.TabIndex = 1;
+            this.chkOutputPin2.Text = "Pin 2";
+            this.chkOutputPin2.UseVisualStyleBackColor = true;
+            this.chkOutputPin2.CheckedChanged += new System.EventHandler(this.chkOutputPin2_CheckedChanged);
+            // 
+            // chkOutputPin3
+            // 
+            this.chkOutputPin3.AutoSize = true;
+            this.chkOutputPin3.Location = new System.Drawing.Point(12, 92);
+            this.chkOutputPin3.Name = "chkOutputPin3";
+            this.chkOutputPin3.Size = new System.Drawing.Size(50, 17);
+            this.chkOutputPin3.TabIndex = 1;
+            this.chkOutputPin3.Text = "Pin 3";
+            this.chkOutputPin3.UseVisualStyleBackColor = true;
+            this.chkOutputPin3.CheckedChanged += new System.EventHandler(this.chkOutputPin3_CheckedChanged);
+            // 
+            // chkOutputPin4
+            // 
+            this.chkOutputPin4.AutoSize = true;
+            this.chkOutputPin4.Location = new System.Drawing.Point(12, 115);
+            this.chkOutputPin4.Name = "chkOutputPin4";
+            this.chkOutputPin4.Size = new System.Drawing.Size(50, 17);
+            this.chkOutputPin4.TabIndex = 1;
+            this.chkOutputPin4.Text = "Pin 4";
+            this.chkOutputPin4.UseVisualStyleBackColor = true;
+            this.chkOutputPin4.CheckedChanged += new System.EventHandler(this.chkOutputPin4_CheckedChanged);
+            // 
+            // chkOutputPin5
+            // 
+            this.chkOutputPin5.AutoSize = true;
+            this.chkOutputPin5.Location = new System.Drawing.Point(12, 138);
+            this.chkOutputPin5.Name = "chkOutputPin5";
+            this.chkOutputPin5.Size = new System.Drawing.Size(50, 17);
+            this.chkOutputPin5.TabIndex = 1;
+            this.chkOutputPin5.Text = "Pin 5";
+            this.chkOutputPin5.UseVisualStyleBackColor = true;
+            this.chkOutputPin5.CheckedChanged += new System.EventHandler(this.chkOutputPin5_CheckedChanged);
+            // 
+            // chkOutputPin6
+            // 
+            this.chkOutputPin6.AutoSize = true;
+            this.chkOutputPin6.Location = new System.Drawing.Point(12, 161);
+            this.chkOutputPin6.Name = "chkOutputPin6";
+            this.chkOutputPin6.Size = new System.Drawing.Size(50, 17);
+            this.chkOutputPin6.TabIndex = 1;
+            this.chkOutputPin6.Text = "Pin 6";
+            this.chkOutputPin6.UseVisualStyleBackColor = true;
+            this.chkOutputPin6.CheckedChanged += new System.EventHandler(this.chkOutputPin6_CheckedChanged);
+            // 
+            // chkOutputPin7
+            // 
+            this.chkOutputPin7.AutoSize = true;
+            this.chkOutputPin7.Location = new System.Drawing.Point(12, 184);
+            this.chkOutputPin7.Name = "chkOutputPin7";
+            this.chkOutputPin7.Size = new System.Drawing.Size(50, 17);
+            this.chkOutputPin7.TabIndex = 1;
+            this.chkOutputPin7.Text = "Pin 7";
+            this.chkOutputPin7.UseVisualStyleBackColor = true;
+            this.chkOutputPin7.CheckedChanged += new System.EventHandler(this.chkOutputPin7_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 231);
+            this.Controls.Add(this.gbxOutputPins);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxOutputPins.ResumeLayout(false);
+            this.gbxOutputPins.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxOutputPins;
+        private System.Windows.Forms.CheckBox chkOutputPin0;
+        private System.Windows.Forms.CheckBox chkOutputPin7;
+        private System.Windows.Forms.CheckBox chkOutputPin6;
+        private System.Windows.Forms.CheckBox chkOutputPin5;
+        private System.Windows.Forms.CheckBox chkOutputPin4;
+        private System.Windows.Forms.CheckBox chkOutputPin3;
+        private System.Windows.Forms.CheckBox chkOutputPin2;
+        private System.Windows.Forms.CheckBox chkOutputPin1;
+    }
+}
+
---
+++ src/Kingsland.PiFaceSharp.SampleClient/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Kingsland.PiFaceSharp.Remote;
+using System.Net;
+
+namespace Kingsland.PiFaceSharp.SampleClient
+{
+
+    public partial class Form1 : Form
+    {
+
+        #region Constructors
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Properties
+
+        private PiFaceTcpClient PiFaceClient
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Form Event Handlers
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var localEndPoint = new IPEndPoint(PiFaceTcpHelper.GetLocalIPAddress(), 43594);
+            var remoteEndPoint = new IPEndPoint(PiFaceTcpHelper.GetLocalIPAddress(), 43596);
+            this.PiFaceClient = new PiFaceTcpClient(localEndPoint, remoteEndPoint);
+            this.PiFaceClient.Connect();
+        }
+
+        #endregion
+
+        #region Output Pin Event Handlers
+
+        private void chkOutputPin0_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PiFaceClient.SetOutputPinState(0, ((CheckBox)sender).Checked);
+        }
+
+        private void chkOutputPin1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PiFaceClient.SetOutputPinState(1, ((CheckBox)sender).Checked);
+        }
+
+        private void chkOutputPin2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PiFaceClient.SetOutputPinState(2, ((CheckBox)sender).Checked);
+        }
+
+        private void chkOutputPin3_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PiFaceClient.SetOutputPinState(3, ((CheckBox)sender).Checked);
+        }
+
+        private void chkOutputPin4_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PiFaceClient.SetOutputPinState(4, ((CheckBox)sender).Checked);
+        }
+
+        private void chkOutputPin5_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PiFaceClient.SetOutputPinState(5, ((CheckBox)sender).Checked);
+        }
+
+        private void chkOutputPin6_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PiFaceClient.SetOutputPinState(6, ((CheckBox)sender).Checked);
+        }
+
+        private void chkOutputPin7_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PiFaceClient.SetOutputPinState(7, ((CheckBox)sender).Checked);
+        }
+
+        #endregion
+
+    }
+
+}
