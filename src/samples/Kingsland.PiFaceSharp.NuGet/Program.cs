f9225a39b7d4cdb8d7435caf545ac92b8e695d52
Michael Clayton <mike@kingslandconsulting.co.uk>
2015-10-02 21:18:00 +0100

    Added Kingsland.PiFaceSharp.NuGet sample program

---
+++ src/samples/Kingsland.PiFaceSharp.NuGet/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Kingsland.PiFaceSharp.Spi;
+using Kingsland.PiFaceSharp.Emulators;
+
+namespace Kingsland.PiFaceSharp.NuGet
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // get reference to default piface device
+            var piface = new PiFaceEmulator();
+
+            // toggle each output pin on and off 50 times
+            for (byte pin = 0; pin < 8; pin++)
+            {
+                Console.WriteLine("toggling input pin {0}", pin);
+                var state = true;
+                for (var i = 0; i < 50; i++)
+                {
+                    piface.SetOutputPinState(pin, state);
+                    System.Threading.Thread.Sleep(25);
+                    state = !state;
+                }
+            }
+
+            // read the current state of each input pin
+            for (byte pin = 0; pin < 8; pin++)
+            {
+                Console.WriteLine("input pin {0} = {1}", pin, piface.GetInputPinState(pin));
+            }
+
+        }
+
+    }
+
+}
